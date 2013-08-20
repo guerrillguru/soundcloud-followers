4620b2bf96b9b39f976908d0d3a7df0749d8c42d
guerrillguru <enth_degree@hotmail.com>
2013-08-20 12:15:17 +1000

    Update sc-unfollowers.js

--- sc-unfollowers.js
+++ sc-unfollowers.js
@@ -44,7 +44,7 @@
  * USAGE:   run this script when you are on https://soundcloud.com/{your username}/following
  */
 
-var SoundcloudUsername = "akerz";
+var SoundcloudUsername = "enthdegree";
 
 var SoundcloudFollowers;
 $.ajax({
